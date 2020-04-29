5e77847f5fcc2e0abd002fb657656578fd84f1a9
Steve Brownlee <chortlehoort@gmail.com>
2020-04-29 08:06:21 -0500

    Updated testing

--- test/farm.test.js
+++ test/farm.test.js
@@ -22,9 +22,9 @@ beforeAll(() => {
     plantedSeeds = usePlants()
 })
 
-describe('Sowing the fields with seeds', () => {
+describe("Seed objects have correct properties", () => {
 
-    test('Corn has correct properties', () => {
+    test("Corn has correct properties", () => {
         expect(createCorn())
             .toMatchObject([
                 { type: "Corn", height: 180, output: 6 },
@@ -32,32 +32,35 @@ describe('Sowing the fields with seeds', () => {
             ])
     })
 
-    test('Potato has correct properties', () => {
+    test("Potato has correct properties", () => {
         expect(createPotato())
             .toMatchObject({ type: "Potato", height: 32, output: 2 })
     })
 
-    test('Asparagus has correct properties', () => {
+    test("Asparagus has correct properties", () => {
         expect(createAsparagus())
             .toMatchObject({ type: "Asparagus", height: 24, output: 4 })
     })
 
-    test('Soybean has correct properties', () => {
+    test("Soybean has correct properties", () => {
         expect(createSoybean())
             .toMatchObject({ type: "Soybean", height: 20, output: 4 })
     })
 
-    test('Sunflower has correct properties', () => {
+    test("Sunflower has correct properties", () => {
         expect(createSunflower())
             .toMatchObject({ type: "Sunflower", height: 380, output: 3 })
     })
 
-    test('Wheat has correct properties', () => {
+    test("Wheat has correct properties", () => {
         expect(createWheat())
             .toMatchObject({ type: "Wheat", height: 230, output: 6 })
     })
 
-    test('Correct type and number of seeds sown', () => {
+})
+
+describe("Sowing the fields with seeds", () => {
+    test("Correct type and number of seeds sown", () => {
         expect(plantedSeeds)
             .toMatchObject(
                 [
@@ -73,78 +76,85 @@ describe('Sowing the fields with seeds', () => {
     })
 })
 
-describe('Harvesting the grown plants', () => {
+describe("Harvesting the grown plants", () => {
     let harvestedPlants
 
     beforeAll(() => {
         harvestedPlants = harvestPlants(plantedSeeds)
     })
 
-    test('Harvested correct number of plants', () => {
+    test("Harvested correct total number of plants", () => {
         expect(harvestedPlants.length).toEqual(25)
+    })
+
+    test("Harvested correct number of asparagus plants", () => {
+        expect(
+            harvestedPlants.filter(p => p.type === "Asparagus").length
+        )
+            .toEqual(4)
+    })
+
+    test("Harvested correct number of corn plants", () => {
+        expect(
+            harvestedPlants.filter(p => p.type === "Corn").length
+        )
+            .toEqual(6)
+    })
+
+    test("Harvested correct number of potato plants", () => {
+        expect(
+            harvestedPlants.filter(p => p.type === "Potato").length
+        )
+            .toEqual(2)
+    })
+
+    test("Harvested correct number of soybean plants", () => {
+        expect(
+            harvestedPlants.filter(p => p.type === "Soybean").length
+        )
+            .toEqual(4)
+    })
+
+    test("Harvested correct number of sunflower plants", () => {
+        expect(
+            harvestedPlants.filter(p => p.type === "Sunflower").length
+        )
+            .toEqual(3)
+    })
+
+    test("Harvested correct number of wheat plants", () => {
+        expect(
+            harvestedPlants.filter(p => p.type === "Wheat").length
+        )
+            .toEqual(6)
     })
 })
 
 
-// [
-//     { type: 'Corn', height: 180, output: 6},
-//     { type: 'Corn', height: 180, output: 6},
-//     { type: 'Corn', height: 180, output: 6},
-//     { type: 'Corn', height: 180, output: 6},
-//     { type: 'Corn', height: 180, output: 6},
-//     { type: 'Corn', height: 180, output: 6},
-//     { type: 'Potato', height: 32, output: 2},
-//     { type: 'Potato', height: 32, output: 2},
-//     { type: 'Asparagus', height: 24, output: 4},
-//     { type: 'Asparagus', height: 24, output: 4},
-//     { type: 'Asparagus', height: 24, output: 4},
-//     { type: 'Asparagus', height: 24, output: 4},
-//     { type: 'Soybean', height: 20, output: 4},
-//     { type: 'Soybean', height: 20, output: 4},
-//     { type: 'Soybean', height: 20, output: 4},
-//     { type: 'Soybean', height: 20, output: 4},
-//     { type: 'Sunflower', height: 380, output: 3},
-//     { type: 'Sunflower', height: 380, output: 3},
-//     { type: 'Sunflower', height: 380, output: 3},
-//     { type: 'Wheat', height: 230, output: 6},
-//     { type: 'Wheat', height: 230, output: 6},
-//     { type: 'Wheat', height: 230, output: 6},
-//     { type: 'Wheat', height: 230, output: 6},
-//     { type: 'Wheat', height: 230, output: 6},
-//     { type: 'Wheat', height: 230, output: 6}
-// ]
+const puppeteer = require("puppeteer")
 
-// const puppeteer = require("puppeteer")
+describe("HTML representation of harvest", () => {
+    test("Harvested plants are rendred to the DOM", async () => {
+        let browser = await puppeteer.launch({
+            headless: true
+        })
+        const page = await browser.newPage();
+        await page.goto("http://localhost:8080");
+        await page.waitForSelector(".plant")
 
-// test('Patients are rendered to DOM', async () => {
-//     let doug = testPerson("Doug", 65, 102)
-//     let mary = testPerson("Mary", 37, 100)
-//     let candace = testPerson("Candace", 42, 103)
+        let section = await page.$eval(".container", _ => _.innerHTML)
 
-//     doug = diganose(doug, 4)
-//     mary = diganose(mary, 2)
-//     candace = diganose(candace, 2)
+        section = section.replace(/\n/g, "")
+        section = section.replace(/\s{2,}/g, "")
 
-//     let browser = await puppeteer.launch({
-//         headless: true
-//     })
-//     const page = await browser.newPage();
-//     await page.goto('http://localhost:8080');
-//     await page.waitForSelector('.patients')
-
-//     let section = await page.$eval('.patients', _ => _.innerHTML)
-
-//     section = section.replace(/\n/g, "")
-//     section = section.replace(/\s{2,}/g, "")
-
-//     try {
-//         expect(section).toBe(`<section class="patient" id="patient--1"><h2 class="patient__name">Doug</h2><div class="patient__properties"><p>Age: 65</p><p>Temperature: 102</p><p>Diagnosed: true</p></div><div class="patient_diagnosis">Infected: true</div></section><section class="patient" id="patient--2"><h2 class="patient__name">Mary</h2><div class="patient__properties"><p>Age: 37</p><p>Temperature: 100</p><p>Diagnosed: true</p></div><div class="patient_diagnosis">Infected: false</div></section><section class="patient" id="patient--3"><h2 class="patient__name">Candace</h2><div class="patient__properties"><p>Age: 42</p><p>Temperature: 103</p><p>Diagnosed: true</p></div><div class="patient_diagnosis">Infected: false</div></section>`);
-//     }
-//     catch (error) {
-//         throw error
-//     }
-//     finally {
-//         browser.close()
-//     }
-
-// })
+        try {
+            expect(section).toBe(`<section class="plant">Corn</section><section class="plant">Corn</section><section class="plant">Corn</section><section class="plant">Corn</section><section class="plant">Corn</section><section class="plant">Corn</section><section class="plant">Potato</section><section class="plant">Potato</section><section class="plant">Asparagus</section><section class="plant">Asparagus</section><section class="plant">Asparagus</section><section class="plant">Asparagus</section><section class="plant">Soybean</section><section class="plant">Soybean</section><section class="plant">Soybean</section><section class="plant">Soybean</section><section class="plant">Sunflower</section><section class="plant">Sunflower</section><section class="plant">Sunflower</section><section class="plant">Wheat</section><section class="plant">Wheat</section><section class="plant">Wheat</section><section class="plant">Wheat</section><section class="plant">Wheat</section><section class="plant">Wheat</section>`);
+        }
+        catch (error) {
+            throw error
+        }
+        finally {
+            browser.close()
+        }
+    })
+})
